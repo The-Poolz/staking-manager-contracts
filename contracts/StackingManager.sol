bf94d8bf23e3f3cfb76a166152a60d2f602529db
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-06-20 10:29:09 +0300

    sender tracker

--- contracts/StackingManager.sol
+++ contracts/StackingManager.sol
@@ -1,70 +1,101 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract StackingManager is Ownable {
     using SafeERC20 for IERC20;
+    using Math for uint256;
 
-    // The stacking vault that will be used for staking and unstaking
-    IERC4626 public stackingVault;
+    // The vault where assets are staked
+    IERC4626 public immutable stackingVault;
 
-    // The token that will be used for staking
-    IERC20 public token;
+    // The token that is being staked
+    IERC20 public immutable token;
 
-    // errors
+    // Mapping of user addresses to their shares in the vault
+    mapping(address => uint256) public userShares;
+
+    // Custom errors for better gas efficiency
     error AmountMustBeGreaterThanZero();
+    error InsufficientShares();
     error ZeroAddress();
 
-    // events
-    event Stake(address indexed user, uint256 shares);
-    event StakingVaultSet(IERC4626 indexed stackingVault, IERC20 indexed token);
-    event Unstake(address indexed user, uint256 shares);
+    // Events to log staking and unstaking actions
+    event Stake(address indexed user, uint256 assets, uint256 shares);
+    event Unstake(address indexed user, uint256 shares, uint256 assets);
 
+    /**
+     * @dev Initializes the contract with the stacking vault address.
+     * @param _stackingVault The address of the IERC4626 vault where assets will be staked.
+     */
     constructor(IERC4626 _stackingVault) Ownable(_msgSender()) {
         if (address(_stackingVault) == address(0)) revert ZeroAddress();
 
         stackingVault = _stackingVault;
         token = IERC20(_stackingVault.asset());
-        emit StakingVaultSet(stackingVault, token);
     }
 
-    /// @notice Sets the stacking vault and token
-    /// @param amount The amount of tokens to stake
-    function stake(uint256 amount) external {
-        if (amount == 0) revert AmountMustBeGreaterThanZero();
+    /**
+     * @dev Allows users to stake assets in the vault.
+     * @param assets The amount of assets to stake.
+     */
+    function stake(uint256 assets) external {
+        if (assets == 0) revert AmountMustBeGreaterThanZero();
 
-        // Transfer the tokens from the sender to this contract
-        IERC20(stackingVault.asset()).safeTransferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
-
-        // Approve the stacking vault to spend the tokens
-        token.forceApprove(address(stackingVault), type(uint256).max);
-        // Deposit the tokens into the stacking vault
-        stackingVault.deposit(amount, msg.sender);
-        // Reset the approval to zero to prevent re-entrancy attacks
+        token.safeTransferFrom(msg.sender, address(this), assets);
+        // Approve the vault to spend the assets        
+        token.forceApprove(address(stackingVault), assets);
+        // Deposit into vault as this contract
+        uint256 shares = stackingVault.deposit(assets, address(this));
+        // Zero out the approval to prevent re-entrancy
         token.forceApprove(address(stackingVault), 0);
 
-        emit Stake(msg.sender, amount);
+        userShares[msg.sender] += shares;
+
+        emit Stake(msg.sender, assets, shares);
     }
 
-    /// @notice Sets the stacking vault
-    /// @param shares The amount of shares to unstake
+    /**
+     * @dev Allows users to unstake their shares and receive the underlying assets.
+     * @param shares The number of shares to unstake.
+     */
     function unstake(uint256 shares) external {
         if (shares == 0) revert AmountMustBeGreaterThanZero();
-        if (shares > stackingVault.balanceOf(msg.sender))
-            revert AmountMustBeGreaterThanZero();
+        if (shares > userShares[msg.sender]) revert InsufficientShares();
 
-        // Withdraw the shares from the stacking vault
-        uint256 amount = stackingVault.withdraw(
+        userShares[msg.sender] -= shares;
+
+        // Withdraw from vault to this contract
+        uint256 assets = stackingVault.redeem(
             shares,
-            msg.sender,
+            address(this),
             address(this)
         );
-        emit Unstake(msg.sender, amount);
+
+        // Send tokens to user
+        token.safeTransfer(msg.sender, assets);
+
+        emit Unstake(msg.sender, shares, assets);
+    }
+
+    /**
+     * @dev Returns the total assets staked by a user.
+     * @param user The address of the user.
+     * @return The total assets staked by the user.
+     */
+    function totalUserAssets(address user) external view returns (uint256) {
+        return stackingVault.previewRedeem(userShares[user]);
+    }
+
+    /**
+     * @dev Returns the total assets in the vault.
+     * @return The total assets in the vault.
+     */
+    function totalAssets() external view returns (uint256) {
+        return stackingVault.previewRedeem(stackingVault.balanceOf(address(this)));
     }
 }
